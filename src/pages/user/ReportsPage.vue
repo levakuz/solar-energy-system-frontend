57bfb8d290ad4282fc4525d21981b1364ad7a661
levakuz <levakuz007@gmail.com>
2023-06-06 17:16:41 +0200

    fixed: reports page

--- src/pages/user/ReportsPage.vue
+++ src/pages/user/ReportsPage.vue
@@ -12,7 +12,7 @@
         v-model="selectedProject"
       />
       <div v-if="selectedProject !== null">
-        <h3>{{ selectedProject.name }}</h3>
+        <h3 class="q-ma-md">{{ selectedProject.name }}</h3>
       </div>
       <div
         v-if="selectedProject !== null"
@@ -21,6 +21,25 @@
         <h4 class="q-ma-md">Date from: {{ selectedProject.dateFrom }}</h4>
         <h4 class="q-ma-md">Date to: {{ selectedProject.dateTo }}</h4>
       </div>
+      <q-select
+        v-if="selectedProject !== null"
+        outlined
+        rounded
+        label="Select device"
+        style="width: 300px"
+        class="q-ma-lg"
+        :options="selectedProject.devices"
+        option-label="name"
+        v-model="selectedDevice"
+      />
+      <h4 v-if="selectedDevice !== null" class="q-ma-md">
+        Total energy produced: {{ selectedDevice.totalEnergyProduced }}
+      </h4>
+      <div
+        v-if="selectedDevice"
+        class="bg-white full-width q-ma-md"
+        style="max-width: 800px; height: 400px"
+      ></div>
     </div>
   </q-page>
 </template>
@@ -35,11 +54,30 @@ export default defineComponent({
   setup() {
     const config = getCurrentInstance().appContext.config.globalProperties;
     const projects = ref([
-      { name: "Test", dateFrom: "12.01.2022", dateTo: "13.01.2022" },
-      { name: "Test1", dateFrom: "12.01.2022", dateTo: "13.01.2022" },
+      {
+        name: "Test",
+        dateFrom: "12.01.2022",
+        dateTo: "13.01.2022",
+        devices: [
+          { name: "a", totalEnergyProduced: "123" },
+          { name: "b", totalEnergyProduced: "222" },
+          { name: "c", totalEnergyProduced: "333" },
+        ],
+      },
+      {
+        name: "Test1",
+        dateFrom: "12.01.2022",
+        dateTo: "13.01.2022",
+        devices: ["d", "e", "f"],
+      },
     ]);
     const selectedProject = ref(null);
-    return { projects, selectedProject };
+    const selectedDevice = ref(null);
+    return {
+      projects,
+      selectedProject,
+      selectedDevice,
+    };
   },
 });
 </script>
